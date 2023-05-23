a2a34a1f5a3e85519c51d61d12cb99dd174e8cc9
jiaoyike <jyk052200@163.com>
2023-05-23 09:58:19 +0800

    first com

---
+++ threeView.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="home-list">
+    <header></header>
+    <main>
+      <p>1</p>
+      <p>2</p>
+      <p>3</p>
+      <p>4</p>
+      <p>5</p>
+      <p>1</p>
+      <p>2</p>
+      <p>3</p>
+      <p>4</p>
+      <p>5</p>
+      <p>1</p>
+      <p>2</p>
+      <p>3</p>
+      <p>4</p>
+    </main>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+.home-list {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  header {
+    height: 50px;
+    border: 1px solid;
+    border-bottom: none;
+  }
+  main {
+    flex: 1;
+    overflow: auto;
+    border: 1px solid;
+  }
+  footer {
+    height: 50px;
+    border: 1px solid;
+    border-top: none;
+  }
+}
+</style>
